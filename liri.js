9176aba28efd24a97a288928f1b286d86260db60
Joed Machado <joedmg58@gmail.com>
2018-07-14 17:07:17 -0400

    Modules needed added

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
 require('dotenv').config();
 require('keys.js');
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+
+var spotify = new Spotify( keys.spotify );
+var client = new Twitter( keys.twitter );
